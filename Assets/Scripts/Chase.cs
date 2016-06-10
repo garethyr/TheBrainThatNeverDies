317c5e6443525bc28241f495a3cfc6a578e55f60
chardzard <richard.sage28@gmail.com>
2016-06-10 19:30:03 -0300

    Mutant chasing and player ded

---
+++ Assets/Scripts/Chase.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chase : MonoBehaviour {
+
+    public float chaseSpeed;
+    public Transform playerPosition;
+
+    Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 move = playerPosition.position;
+        move.y = transform.position.y;
+        transform.position = Vector2.Lerp(transform.position, move, chaseSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        print("hit");
+        if (other.gameObject.CompareTag("Mutant")) {
+            StartCoroutine(Dodge());
+        }
+    }
+
+    IEnumerator Dodge() {
+        yield return new WaitForSeconds(3f);
+        print("dead");
+    }
+}
