4eb7d7b1d929ea517d7af4474a594f75be17acc9
Gareth Yawching-Rickwood <garethrickwood@hotmail.com>
2016-06-10 19:27:33 -0300

    Basic setup for aliens and women and game controller and stuff.

---
+++ Assets/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    public Transform WomanTransform;
+    public Transform AlienTransform;
+
+    public Transform FemaleSpawnPoint;
+
+    public float AlienChance;
+    public int MaxFemales;
+    public float SpawnCooldown;
+
+    private Transform[] females;
+
+
+	// Use this for initialization
+	void Start () {
+        females = new Transform[MaxFemales];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (SpawnCooldown > 0) {
+            SpawnCooldown--;
+        } else {
+            doSpawn();
+        }
+	}
+
+    void doSpawn() {
+        
+
+
+        if (Random.value < AlienChance) {
+
+        }
+    }
+}
